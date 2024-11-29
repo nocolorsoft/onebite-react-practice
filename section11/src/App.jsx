5d9e8bfcf95409bd793236d50847b06935d9db85
nocolorsoft <nocolorsoft@gmail.com>
2024-11-29 20:41:06 +0900

    Add solutions up to OneBite class #77

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
-import {useState, useRef, useReducer, useCallback} from 'react';
+import {useState, useRef, useReducer, useCallback, createContext} from 'react';
 import Header from './components/Header.jsx';
 import List from './components/List.jsx';
 import Editor from './components/Editor.jsx';
 
 const mockData = [
-    {id:0, isDone: false, content: 'React 공부하기', date: new Date().getTime()},
-    {id:1, isDone: false, content: '빨래하기', date: new Date().getTime()},
-    {id:2, isDone: false, content: '노래 연습하기', date: new Date().getTime()},
+    {id: 0, isDone: false, content: 'React 공부하기', date: new Date().getTime()},
+    {id: 1, isDone: false, content: '빨래하기', date: new Date().getTime()},
+    {id: 2, isDone: false, content: '노래 연습하기', date: new Date().getTime()},
 ];
 
 function reducer(state, action) {
@@ -24,29 +24,32 @@ function reducer(state, action) {
     }
 }
 
+export const TodoContext = createContext();
+
+
 function App() {
     const [todos, dispatch] = useReducer(reducer, mockData);
     const idRef = useRef(3);
 
     const onCreate = useCallback((content) => {
         dispatch({
-            type : 'CREATE',
-            data : {
-                id : idRef.current ++,
+            type: 'CREATE',
+            data: {
+                id: idRef.current++,
                 isDone: false,
                 content: content,
-                date : new Date().getTime()
+                date: new Date().getTime()
             }
         });
-    },[]);
+    }, []);
 
     const onUpdate = useCallback((targetId) => {
         console.log(targetId);
         dispatch({
-            type : 'UPDATE',
-            targetId : targetId
+            type: 'UPDATE',
+            targetId: targetId
         });
-    },[]);
+    }, []);
 
 
     const onDelete = useCallback((targetId) => {
@@ -59,9 +62,11 @@ function App() {
     return (
         <>
             <div className="App">
-                <Header />
-                <Editor onCreate={onCreate} />
-                <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
+                <Header/>
+                <TodoContext.Provider value={{todos, onCreate, onUpdate, onDelete}}>
+                    <Editor/>
+                    <List />
+                </TodoContext.Provider>
             </div>
         </>
     );
